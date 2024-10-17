b3cb073ada91f244c3057db3d307c9fd7fe156ba
tsboudot <tsboudot@gmail.com>
2024-10-17 17:35:57 +0200

    handleSearch & NavigateTOSearch Implémentés et fonctionnels dans la searchBar

--- front/src/components/header/SearchBar.tsx
+++ front/src/components/header/SearchBar.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { navigateToSearch } from '../../utils/navigateToSearch';
 
-const searchBar = () => {
+const SearchBar = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('calling handleSearch with:', searchTerm);
+        navigateToSearch(navigate, searchTerm);
+
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    };
+
     return (
-        <div className='w-3/5'>
-            <input className='w-full h-full bg-white rounded' type='text'></input>
-        </div>
+        <form className='flex flex-row' onSubmit={handleSearch}>
+            <input
+                className='w-3/5 h-full bg-white rounded'
+                type='text'
+                value={searchTerm}
+                onChange={handleInputChange}
+                placeholder="Tapez votre recherche ici..."
+            />
+            <button className='w-2/5' type="submit">Rechercher</button>
+        </form>
     );
 };
 
-export default searchBar;
+export default SearchBar;
