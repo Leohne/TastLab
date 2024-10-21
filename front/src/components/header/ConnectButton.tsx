49f47433ed0ee2674c8df6e558beb349aa1d5533
tsboudot <tsboudot@gmail.com>
2024-10-21 13:16:47 +0200

    Actualisation des connectButton + navigateToSearch suite à MaJ du router

--- front/src/components/header/ConnectButton.tsx
+++ front/src/components/header/ConnectButton.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ConnectButton = () => {
     return (
         <div className=' w-1/5'>
-            <Link to="/connexion"><button className='bg-white-200'>Connectez Vous</button></Link>
+            <Link to="/login"><button className='bg-white-200'>Connectez Vous</button></Link>
         </div>
     );
 };
--- front/src/utils/navigateToSearch.ts
+++ front/src/utils/navigateToSearch.ts
@@ -1,6 +1,6 @@
 import { NavigateFunction } from 'react-router-dom';
 
 export const navigateToSearch = (navigate: NavigateFunction, searchTerm: string) => {
-    navigate(`/recherche?terms=${encodeURIComponent(searchTerm)}`);
+    navigate(`/search?terms=${encodeURIComponent(searchTerm)}`);
     console.log(searchTerm);
 };
