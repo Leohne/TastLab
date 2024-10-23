06fd30bb1cda6151a7de1484477433bdb06f2fd0
Leohne <bricemagneville.pro@gmail.com>
2024-10-23 15:52:14 +0200

    css login + searchBar

--- front/src/components/header/SearchBar.tsx
+++ front/src/components/header/SearchBar.tsx
@@ -19,9 +19,9 @@ const SearchBar = () => {
     };
 
     return (
-        <form className='flex flex-row' onSubmit={handleSearch}>
+        <form className='flex w-1/4 flex-row' onSubmit={handleSearch}>
             <input
-                className='w-3/5 h-full bg-white rounded p-2'
+                className=' w-2/4 bg-white rounded p-2'
                 type='text'
                 value={searchTerm}
                 onChange={handleInputChange}
--- front/src/pages/connexion/Connexion.tsx
+++ front/src/pages/connexion/Connexion.tsx
@@ -27,18 +27,20 @@ function Connexion() {
 
 
     return (<>
-        <div>
+        <div className='flex justify-center'>
+            <div className='flex flex-col w-1/4 p-5 my-20 text-center bg-white rounded-[16px] '> 
+                <h2 className='font-Sugar text-4xl text-[#341f16] py-7'>Connexion</h2>               
+                <form onSubmit={handleLogin} className='flex flex-col'>
+                    <input className='form' type="email" placeholder="Utilisateur / Email" value={userLogin} onChange={(e) => setUserLogin(e.target.value)} />
+                    <input className='form' type="password" placeholder='Mot de passe' value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+                    <button className='text-[#341f16] w-1/5 mx-auto rounded-[10px] hover:bg-[#CB9D6C] hover:text-white ' type="submit">Se connecter</button>
+                </form>
 
-            <form onSubmit={handleLogin}>
-                <input type="email" placeholder="votre email" value={userLogin} onChange={(e) => setUserLogin(e.target.value)} />
-                <input type="password" placeholder='Votre mot de passe' value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
-                <button type="submit">Se connecter</button>
-            </form>
 
-
-            <div className="justify-self justify-center">
-                <p>Not a user ? </p>
-                <a href="/sign-up"> Create an account</a>
+                <div className="justify-self justify-center mt-5">
+                    <p>Not a user ? </p>
+                    <a href="/sign-up"> Create an account</a>
+                </div>
             </div>
         </div>
     </>
