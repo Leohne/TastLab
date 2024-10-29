5a23fd385081e3b6e441765e6f83cf6b9c4f8449
Leohne <bricemagneville.pro@gmail.com>
2024-10-29 17:29:30 +0100

    design card recherche + animation (en cours)

--- front/src/components/page/recetteview/RecetteView.tsx
+++ front/src/components/page/recetteview/RecetteView.tsx
@@ -18,13 +18,25 @@ const RecetteView: React.FC<{ plat: plats[]; isGrid: boolean }> = ({ plat, isGri
                 <div className={`flex p-10 ${isGrid ? 'w_grid' : 'w_list'}`} key={plats.id}>
                     <Link to="/recette" /*to={`/receipe/${item.id}`}*/ className={`${isGrid ? '' : 'a_list'}`}>
                     <div className={`${isGrid ? 'cardGrid' : 'cardList'}`}>
-                        <div className={`${isGrid ? 'img_grid' : 'img_list'}`}>
-                            <img src={plats.cover} alt="image plat" className='object-cover w-[290px] h-[400px] rounded-[15px]' />
+                        <div className={`${isGrid ? 'relative' : 'img_list'}`}>
+                            <img src={plats.cover} alt="image plat" className='object-cover w-full h-[400px] rounded-[15px]' />
+                            <div className={`${isGrid ? 'p_grid' : 'hidden'}`}>
+                                <p className='anim_grid'>Le bibimbap est un plat coréen composé de riz blanc recouvert de légumes variés, 
+                                   de viande (souvent du bœuf), d'un œuf et d'une sauce épicée au gochujang. Tous les ingrédients
+                                   sont mélangés avant d'être dégustés, ce qui combine leurs saveurs et textures pour un repas à la fois équilibré, coloré et savoureux.
+                                </p>
+                            </div>
                         </div>                        
                         <div className='flex flex-col justify-between w-[60%] pt-auto py-2'>
-                            <div className="font-Sugar pl-2 text-2xl">{plats.nom}</div>
-                            <div className="flex justify-start">
-                                <div className="flex flex-row mx-1"> <img src={timeIcone} alt="icone temps" className='mr-2' /> {plats.temps}</div>
+                            <div className={`font-Sugar text-2xl ${isGrid ? 'text-center' : 'pl-2'}`}>{plats.nom}</div>
+                            <div>
+                                <p className={`${isGrid ? 'hidden' : 'pl-2'}`}>Le bibimbap est un plat coréen composé de riz blanc recouvert de légumes variés, 
+                                   de viande (souvent du bœuf), d'un œuf et d'une sauce épicée au gochujang. Tous les ingrédients
+                                   sont mélangés avant d'être dégustés, ce qui combine leurs saveurs et textures pour un repas à la fois équilibré, coloré et savoureux.
+                                </p>
+                            </div>
+                            <div className={`flex ${isGrid ? 'justify-center' : 'justify-start'}`}>
+                                <div className="flex flex-row mx-1"> <img src={timeIcone} alt="icone temps" className='mr-2'/> {plats.temps}</div>
                                 <div className="flex ml-4">
                                 {Array.from({ length: plats.note }, (_, index) => {
                                  return <img key={index} src={star} alt="star rate"/> ;
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -11,8 +13,20 @@ export default {
         'Sugar':['"Sugar"'],
         'Itim':['"Itim"'],
         'sans':['"Louis cafe"']
-      }
+      },
+      animation: {
+        drop : 'drop 0.6s ease-in-out',
+      },
+      keyframes: {
+        drop: {
+          '0%': { transform: 'translateY(-150%)', opacity: '0'},
+          '50%': {opacity:'0.1'},
+          '80%': {opacity:'0.6'},
+          '100%': { transform: 'translateY(0)', opacity:'1' },
+        }
+        }
+      },
     },
-  },
-  plugins: [],
-}
+  };
+  plugins: []
+
