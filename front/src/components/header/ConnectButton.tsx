af437f95c648367c9b98b08432977972d8019dd8
Leohne <bricemagneville.pro@gmail.com>
2024-11-04 15:36:28 +0100

    close menu clickOut

--- front/src/components/header/ConnectButton.tsx
+++ front/src/components/header/ConnectButton.tsx
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const ConnectButton = () => {
     const [connected, setconnected] = useState(true)
     const [isExpanded, setIsExpanded] = useState(false)
+    const clickOut = useRef<HTMLButtonElement>(null);
+
+    const handleClickOutside = (event: MouseEvent) => {
+        
+        if (clickOut.current && !clickOut.current.contains(event.target as Node)) {
+            setIsExpanded(false);
+        }
+    };
+
+    useEffect(() => {
+                document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
     
     return (
         <div className='pr-2 w-1/5'>
@@ -13,7 +28,7 @@ const ConnectButton = () => {
                 </div>
             </Link>
             <div className={`text-center ${connected ? '' : 'hidden'}`}>
-                <button aria-label="user" onClick={() => setIsExpanded(!isExpanded)}  className='py-3 pr-4 bg-white-200 hover:text-white'>Username</button> {
+                <button aria-label="user" ref={clickOut} onClick={() => setIsExpanded(!isExpanded)}  className='py-3 pr-4 bg-white-200 hover:text-white'>Username</button> {
                 isExpanded && <ul className='mt-4 rounded-[16px] bg-[#CB9D6C] text-white relative z-10'>
                 <li>
                     <Link to="/account"> {/* ajouter /:id */}
