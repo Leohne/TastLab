c7502de2701baf9cbc609401245401883765e26a
Leohne <bricemagneville.pro@gmail.com>
2024-11-07 12:25:48 +0100

    add recipe ( en cours )

--- front/src/pages/add_recipe/Add_recipe.tsx
+++ front/src/pages/add_recipe/Add_recipe.tsx
@@ -1,9 +1,15 @@
+import img_empty from "/asset/icone/img_empty2.jpg"
+
 function Add_recipe () {
     return (
-        <div>
-            <form action="submit">
+        <div className="flex">
+            <form action="submit" className="flex flex-col mx-auto">
                 <label htmlFor="title">Titre</label>
-                <input type="text" />
+                <input aria-label="text pour le titre" type="text" id="title" />
+                <label htmlFor="img_upload">Image de présentation
+                <img src={img_empty} alt="ajout d'image" />
+                </label>
+                <input aria-label="ajout image" type="file" id="img_upload" className="opacity-0" />
             </form>
         </div>        
     )
