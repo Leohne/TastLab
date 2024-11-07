778b3d879d81ca4d8efd726705de5e0443434bd4
Leohne <bricemagneville.pro@gmail.com>
2024-11-07 15:13:01 +0100

    preview img

--- front/src/components/page/step_recipe/Step_recipe.tsx
+++ front/src/components/page/step_recipe/Step_recipe.tsx
@@ -1,23 +1,53 @@
-function Step_recipe() {
-    const renderImage = (imgSrc?: string) => {
-        return imgSrc ? <img src={imgSrc} alt="step_img" /> : '';
-      };
-    return (
-        <>
-        <h3></h3>
-        <div>
-            {renderImage}
-        </div>
-        <div>
-            <div>
+import { useState } from "react";
+import img_icone from "/asset/icone/img_empty2.jpg"
 
-            </div>
-            <div>
-                
-            </div>
+function Step_recipe( {onClose} ) {
+
+   /* const [imgSrc, setimgSrc] = useState<string | null>(null);
+    const prevision = useRef<HTMLDivElement>(null);
+
+    const readUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        const files = e.target.files;
+        if (files && files.length > 0) {
+          const [picture] = files;
+          if (picture) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+              if (prevision.current && event.target) {
+                setimgSrc(event.target.result as string);
+              }
+            };    
+            reader.readAsDataURL(picture);
+          }
+        }
+      };*/
+
+    return (
+        <div>
+            <h2>Salut grosse pute !!</h2>
         </div>
-        </>
-    )
+        /*
+        <div className="flex">
+            <form action="submit" method="post" className="flex flex-col mx-auto w-2/4">
+                <label htmlFor="title">Titre</label>
+                <input aria-label="text pour le titre" type="text" id="title" />
+                <label htmlFor="img_upload">Image de présentation
+                <div ref={prevision} className="w-4/5" >
+                {imgSrc ? (
+                    <img src={imgSrc} alt="Prévisualisation" className="object-cover w-full h-[400px]"/>
+                ) : (
+                    <img src={img_icone} alt="Prévisualisation" className="object-cover w-full h-[400px]"/>
+                )}
+                </div>
+                </label>
+                <input aria-label="ajout image" type="file" id="img_upload" onChange={readUrl} className="opacity-0" />
+                <button>Ajouter</button>
+
+            </form>
+        </div>       
+         */ 
+   )
 }
 
 export default Step_recipe
--- front/src/pages/add_recipe/Add_recipe.tsx
+++ front/src/pages/add_recipe/Add_recipe.tsx
@@ -1,16 +1,51 @@
-import img_empty from "/asset/icone/img_empty2.jpg"
+import { useRef, useState } from "react";
+import img_icone from "/asset/icone/img_empty2.jpg"
+import Step_recipe from "../../components/page/step_recipe/Step_recipe";
 
 function Add_recipe () {
+
+    const prevision = useRef<HTMLDivElement>(null);
+    const [imgSrc, setimgSrc] = useState<string | null>(null);
+    const [isExpanded, setIsExpanded] = useState(false)
+     
+    const readUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        const files = e.target.files;
+        if (files && files.length > 0) {
+          const [picture] = files;
+          if (picture) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+              if (prevision.current && event.target) {
+                setimgSrc(event.target.result as string);
+              }
+            };    
+            reader.readAsDataURL(picture);
+          }
+        }
+      };
+
     return (
-        <div className="flex">
-            <form action="submit" className="flex flex-col mx-auto">
+        <div className="flex flex-col">
+            <form method="post" className="flex flex-col mx-auto w-2/4">
                 <label htmlFor="title">Titre</label>
                 <input aria-label="text pour le titre" type="text" id="title" />
                 <label htmlFor="img_upload">Image de présentation
-                <img src={img_empty} alt="ajout d'image" />
+                <div ref={prevision} className="w-4/5" >
+                {imgSrc ? (
+                    <img src={imgSrc} alt="Prévisualisation" className="object-cover w-full h-[400px]"/>
+                ) : (
+                    <img src={img_icone} alt="Prévisualisation" className="object-cover w-full h-[400px]"/>
+                )}
+                </div>
                 </label>
-                <input aria-label="ajout image" type="file" id="img_upload" className="opacity-0" />
+                <input aria-label="ajout image" type="file" id="img_upload" onChange={readUrl} className="opacity-0" />
             </form>
+                <button>Ajoutez une étape</button>
+                <div>
+                {isExpanded && <Step_recipe onClose={() => setIsExpanded(false)}/>}
+                </div> 
+            <button type="submit">Envoyer</button>
         </div>        
     )
 }
