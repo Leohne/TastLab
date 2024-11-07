ba7bab10335985c93d3d74218afe71b6f9b0ad76
tsboudot <tsboudot@gmail.com>
2024-11-07 17:12:22 +0100

    Refacto middleware Recette Ok // To do : middleWare validation Recettes

---
+++ serveur/src/middlewares/recettes/index.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import db from '../../knex'
+import { recetteInfos, etapeRecette } from "../../types/recettes/interfacesRecettes";
+
+export const getAllRecettes = async (req: Request, res: Response) => {
+    try {
+        const recettes: recetteInfos[] = await db<recetteInfos>('recipes').select('*')
+        res.json(recettes);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur du serveur lors de la récupération des recettes');
+    }
+}
+
+export const searchRecipeByFreeText = async (req: Request, res: Response) => {
+    const { searchTerm } = req.query
+    try {
+        const recettes: recetteInfos[] = await db<recetteInfos>('recipes').select('*')
+            .where('title', 'like', 'searchTerm')
+            .orWhere('description', 'like', 'searchTerm')
+        res.json(recettes)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur du serveur lors de la récupération des recettes');
+    }
+}
+
+export const fetchRecetteStep = async (req: Request, res: Response) => {
+    const reqId = req.params.id;
+    try {
+        const step: etapeRecette[] = await db<etapeRecette>('recipes_step')
+            .select('*')
+            .where('recipe_id', reqId)
+            .orderBy('step_index', 'asc')
+        res.json(step);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Erreur du serveur lors de la récupération des recettes');
+    }
+}
