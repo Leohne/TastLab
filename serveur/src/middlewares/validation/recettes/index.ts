3d7af859fe0009c446ed1c01faa26d7dc3c3acaf
tsboudot <tsboudot@gmail.com>
2024-11-08 16:56:54 +0100

    MiddleWare de validations : SearchTerm must exist && RecetteID must exist. Else => Cheh

--- serveur/src/middlewares/validation/recettes/index.ts
+++ serveur/src/middlewares/validation/recettes/index.ts
@@ -1,1 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+
+export const validateSearchTerm = (req: Request, res: Response, next: NextFunction) => {
+    const { searchTerm } = req.query;
+    if (!searchTerm) {
+        console.log('cheh ! En plein dans ta grosse mouille')
+        res.status(400).send('Le paramètre "searchTerm" est requis');
+        return
+    }
+    next();
+};
+
+export const validateRecetteId = (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    if (!id) {
+        console.log('cheh ! En plein dans ta grosse mouille')
+        res.status(400).send("L'ID de la recette est requis");
+        return
+    }
+    next();
+};
--- serveur/src/routes/recettes/index.ts
+++ serveur/src/routes/recettes/index.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express';
 import { fetchRecetteStep, getAllRecettes, searchRecipeByFreeText } from '../../middlewares/recettes';
-
+import { validateSearchTerm, validateRecetteId } from '../../middlewares/validation/recettes/index';
 
 const router = express.Router();
 
 
 router.get('/recettes', getAllRecettes)
-router.get('/recettes/search', searchRecipeByFreeText)
-router.get('/recette/:id', fetchRecetteStep)
+router.get('/recettes/search', validateSearchTerm, searchRecipeByFreeText)
+router.get('/recette/:id', validateRecetteId, fetchRecetteStep)
 
 
 export default router;
